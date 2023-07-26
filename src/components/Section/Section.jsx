5791a89afc3b86584e9f91bbaa6fbcaf2330c886
maryna-haliieva <marina.galieva7@gmail.com>
2023-07-26 18:21:15 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types';
 import { SectionBox, SectionTitle } from './Section.styled';
 
-export default function Section(title, children) {
+export default function Section({ title, children }) {
   return (
     <SectionBox>
       {title && <SectionTitle>{title}</SectionTitle>}
       {children}
-      {/* <h2>Please leave feedback</h2> */}
     </SectionBox>
-    // <section>
-    //   <h2>Please leave feedback</h2>
-    // </section>
   );
 }
 
